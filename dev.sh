c6620c4cdc862f7d869d5f9355a3a87e8b2fc455
Chris Langager <chris.langager@logrhythm.com>
2016-04-09 19:43:19 -0600

    data type design notes

--- dev.sh
+++ dev.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 docker build -t alternative-vote/server .
-docker run -p 3000:3000 -v `pwd`:/usr/src/app/ -d --name=server  alternative-vote/server
+docker run -p 3000:3000 -v `pwd`:/usr/src/app/ -it --rm --name=server  alternative-vote/server
